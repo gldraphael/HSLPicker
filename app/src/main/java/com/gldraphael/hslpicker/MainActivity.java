7263e1efc4adf8f38bb094d7970aa390b9912737
Galdin Raphael <gldraphael@gmail.com>
2016-03-19 06:47:30 +0530

    Update the UI when the sliders are moved.
    Could probably be done better. If only the databinding API supports two-way binding.

--- app/src/main/java/com/gldraphael/hslpicker/MainActivity.java
+++ app/src/main/java/com/gldraphael/hslpicker/MainActivity.java
@@ -1,11 +1,18 @@
 package com.gldraphael.hslpicker;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v7.app.AppCompatActivity;
 
-import android.databinding.DataBindingUtil;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+
+import com.gldraphael.hslpicker.BR;
 
 import com.gldraphael.hslpicker.databinding.ContentMainBinding;
 
@@ -14,22 +21,93 @@ public class MainActivity extends AppCompatActivity {
     /**
      * The main activity ViewModel
      */
-    public class HslVM {
-        public int hue = 192;
-        public int saturation = 100;
-        public int lightness = 50;
+    public class HslVM extends BaseObservable {
+
+        private IOnColorsUpdated eventHandler;
+
+        private int hue = 192;
+        private int saturation = 0;
+        private int lightness = 0;
+
+        public HslVM(IOnColorsUpdated eventHandler){
+            this.eventHandler = eventHandler;
+        }
 
         public String getHex(){
             return "#CCC";
         }
 
         public int getColor(){
-            return ColorUtils.HSLToColor(new float[] { hue, saturation * 0.01f, lightness * 0.01f});
+            return ColorUtils.HSLToColor(new float[] {
+                    getHue(),
+                    getSaturation() * 0.01f,
+                    getLightness() * 0.01f});
+        }
+
+        @Override
+        public synchronized void notifyChange() {
+            super.notifyChange();
+            eventHandler.onColorsUpdated();
+            Log.d("###", "notifyChange");
+        }
+
+        @Override
+        public void notifyPropertyChanged(int fieldId) {
+            super.notifyPropertyChanged(fieldId);
+
+            eventHandler.onColorsUpdated();
+            Log.d("###", "propertyChange");
+        }
+
+        @Bindable
+        public int getHue() {
+            return hue;
+        }
+
+        public void setHue(int hue) {
+            this.hue = hue;
+            notifyPropertyChanged(BR.hue);
+        }
+
+        @Bindable
+        public int getSaturation() {
+            return saturation;
+        }
+
+        public void setSaturation(int saturation) {
+            this.saturation = saturation;
+            notifyPropertyChanged(BR.saturation);
+        }
+
+        @Bindable
+        public int getLightness() {
+            return lightness;
+        }
+
+        public void setLightness(int lightness) {
+            this.lightness = lightness;
+            notifyPropertyChanged(BR.lightness);
+        }
+
+        @Override
+        public String toString() {
+            return "HSL(" + hue + ", " + saturation + ", " + lightness + ")";
         }
     }
 
-    private HslVM hsl = new HslVM();
+    public interface IOnColorsUpdated {
+        void onColorsUpdated();
+    }
+
+    private HslVM hsl = new HslVM(new IOnColorsUpdated() {
+        @Override
+        public void onColorsUpdated() {
+            updateBackground();
+            Log.d("###", "Hey!");
+        }
+    });
     private View content = null;
+    ContentMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +116,68 @@ public class MainActivity extends AppCompatActivity {
         content = findViewById(android.R.id.content);
 
         // Configure databinding
-        ContentMainBinding binding = DataBindingUtil.setContentView(this, R.layout.content_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.content_main);
         binding.setHslvm(hsl);
         updateBackground();
+
+        binding.sbHue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                hsl.setHue(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.sbSaturation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                hsl.setSaturation(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        binding.sbLightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                hsl.setLightness(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     private void updateBackground(){
+        Log.d("###", "Updating background to " + hsl.toString());
         content.setBackgroundColor(hsl.getColor());
+    }
+
+    public void onClickSlider(View view) {
+        updateBackground();
     }
 }
